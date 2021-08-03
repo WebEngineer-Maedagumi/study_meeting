0be49071e788b7f1fe60bced9e0604912abb7819
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2021-08-03 09:08:56 +0900

    fixed js study

--- study_meeting2/index.js
+++ study_meeting2/index.js
@@ -1,11 +1,14 @@
-
 //コンソールでHello World
-console.log("hello world")
+console.log ( "hello world" )
+
+const hello = "hello world"//helloに"hello world"を代入
+
+console.log ( hello )
 
 //ブラウザにHello World
-function helloWorld() {
-    const hello =　document.getElementById("hello")//htmlで取得したidを取得
-    hello.innerText = "Hello World"
+function helloWorld () {
+  const hello = document.getElementById ( "hello" )//htmlで取得したidを取得
+  hello.innerText = "Hello World"
 }
 
-helloWorld()
+helloWorld ()
